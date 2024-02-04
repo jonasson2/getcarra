1a3559cff4e905663b42067e04124974036b3786
Kristján Jónasson <jonasson@hi.is>
2024-02-04 22:16:23 +0000

    Bjó til föll úr skipunum sem voru í "main" hluta get_carra.py og stytti "main" niður í 10 línur eða svo

--- get_carra.py
+++ get_carra.py
@@ -3,38 +3,38 @@ import numpy as np, pygrib, datetime, json, pandas as pd, time, cdsapi
 import tempfile, os
 from datetime import datetime, timedelta
 
-def retrieve_specific_time(timestamp:str, hl: list[str], vbs: list[str],
-                           product_type: str):
-    grib_file = tempfile.mktemp() + ".grib"
-    print("grib_file:", grib_file)
-    dt_obj = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S")
-    yr = dt_obj.year
-    mt = dt_obj.month
-    day = dt_obj.day
-    hr = dt_obj.hour
-    input_dict = {
-        'domain':       'west_domain',
-        'variable':     vbs,
-        'height_level': hl,
-        'product_type': product_type,
-        'time':         hr,
-        'year':         yr,
-        'month':        mt,
-        'day':          day,
-        'format':       'grib',
-        'grid':         [0.04180602, 0.01826484],
-        'area':         [66.6, -24.6, 63.3, -13.4]
-    }
-    if product_type == 'forecast':
-        input_dict.update({'leadtime_hour': '24'})
-    cdsapi.Client().retrieve('reanalysis-carra-height-levels', input_dict, grib_file)
-    running = True
-    while running:
-        time.sleep(1)
-        print("Running>>>>")
-        running = not os.path.exists(grib_file)
-    print("Out of while loop")
-    return grib_file
+# def retrieve_specific_time(timestamp:str, hl: list[str], vbs: list[str],
+#                            product_type: str):
+#     grib_file = tempfile.mktemp() + ".grib"
+#     print("grib_file:", grib_file)
+#     dt_obj = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S")
+#     yr = dt_obj.year
+#     mt = dt_obj.month
+#     day = dt_obj.day
+#     hr = dt_obj.hour
+#     input_dict = {
+#         'domain':       'west_domain',
+#         'variable':     vbs,
+#         'height_level': hl,
+#         'product_type': product_type,
+#         'time':         hr,
+#         'year':         yr,
+#         'month':        mt,
+#         'day':          day,
+#         'format':       'grib',
+#         'grid':         [0.04180602, 0.01826484],
+#         'area':         [66.6, -24.6, 63.3, -13.4]
+#     }
+#     if product_type == 'forecast':
+#         input_dict.update({'leadtime_hour': '24'})
+#     cdsapi.Client().retrieve('reanalysis-carra-height-levels', input_dict, grib_file)
+#     running = True
+#     while running:
+#         time.sleep(1)
+#         print("Running>>>>")
+#         running = not os.path.exists(grib_file)
+#     print("Out of while loop")
+#     return grib_file
 
 def retrieve_month(vbs: list[str], height_levels: list[int], product_type: str,
                    yrmonth:str, days: list[int], hr_list: list[int]):
@@ -66,8 +66,11 @@ def retrieve_month(vbs: list[str], height_levels: list[int], product_type: str,
     print("Out of while loop")
     return grib_file
 
+def date_format():
+    return "%Y-%m-%dT%H:%M:%S"
+
 def add3hrs(date):
-    fmt = "%Y-%m-%dT%H:%M:%S"
+    fmt = date_format()
     dt_obj = datetime.strptime(date, fmt) + timedelta(hours=3)
     return dt_obj.strftime(fmt)
 
@@ -77,7 +80,7 @@ def grib_latlon(grib_file):
         (lat, lon) = message.latlons()
     return (np.flipud(lat), lon - 360)
 
-def read_grib(grib_file, vbs: list[str], height_levels: list[int], 
+def read_grib(grib_file, vbs: list[str], height_levels: list[int],
               days: list[int], hr_list: list[int]):
     (lat_grid, lon_grid) = grib_latlon(grib_file)
     (nlat, nlon) = lat_grid.shape
@@ -133,31 +136,29 @@ def get_carra_param(file_path):
         timestamp_location = entry["timestamp_location"]
     return carra_dict, timestamp_location
 
-file_path = 'carra_param.json'
-carra_dict, timestamp_location = get_carra_param(file_path)
-var_list = carra_dict["variable"]
-height_lev = carra_dict["height_levels"]
-product_type = carra_dict["product_type"]
+def construct_year_month_set(timestamp_location):
+    # Create set of year-month for which to retrieve data
+    yr_month = set()
+    fmt = date_format()
+    for date in timestamp_location.keys():
+        dt_obj = datetime.strptime(date, fmt)
+        yr_month.add(date[:7])
+        if dt_obj.hour >= 21:
+            next_dt = dt_obj + timedelta(days=1)
+            dt_obj = datetime.strptime(date, fmt) + timedelta(hours=3)
+            next_day = dt_obj.strftime(fmt)
+            yr_month.add(next_day[:7])
+    return yr_month
 
-df = pd.DataFrame()
-
-# Create set of year-month for which to retrieve data
-yr_month = set()
-fmt = "%Y-%m-%dT%H:%M:%S"
-for date in timestamp_location.keys():
-    dt_obj = datetime.strptime(date, fmt)
-    yr_month.add(date[:7])
-    if dt_obj.hour >= 21:
-        next_dt = dt_obj + timedelta(days=1)
-        dt_obj = datetime.strptime(date, fmt) + timedelta(hours=3)
-        next_day = dt_obj.strftime(fmt)
-        yr_month.add(next_day[:7])
-
-hr_list = [12] if product_type == "forecast" else [0, 3, 6, 9, 12, 15, 18, 21]
-
-for month in yr_month:
-    days = [int(d[8:10]) for d in timestamp_location.keys() if d[:7] == month]
-    grib_file = retrieve_month(var_list, height_lev, product_type, month, days, hr_list)
+def get_month(df, carra_dict, timestamp_location, yr_month):
+    # Gets one month from Carra and adds the retrieved info to the data frame df
+    fmt = date_format()
+    var_list = carra_dict["variable"]
+    height_lev = carra_dict["height_levels"]
+    product_type = carra_dict["product_type"]
+    hr_list = [12] if product_type == "forecast" else [0, 3, 6, 9, 12, 15, 18, 21]
+    days = [int(d[8:10]) for d in timestamp_location.keys() if d[:7] == yr_month]
+    grib_file = retrieve_month(var_list, height_lev, product_type, yr_month, days, hr_list)
     res, lat_grid, lon_grid = read_grib(grib_file, var_list, height_lev, days, hr_list)
     for timestamp in timestamp_location.keys():
         dt_obj = datetime.strptime(timestamp, fmt)
@@ -190,6 +191,14 @@ for month in yr_month:
                 for v in var_list:
                     df_row[v] = values[v][kh]
                 df = pd.concat([df, pd.DataFrame([df_row])], ignore_index=True, axis=0)
+        return df
+
+json_file = 'carra_param.json'
+carra_dict, timestamp_location = get_carra_param(json_file)
+df = pd.DataFrame()
+yr_month_set = construct_year_month_set(timestamp_location)
+for yr_month in yr_month_set:
+    df = get_month(df, carra_dict, timestamp_location, yr_month)
 
 filename = "prufa.feather"
 df.to_feather(filename)
